c792d8f7c24fca84340a4f51744bd0723c166fdf
Owillz01 <omonkhodion@gmail.com>
2024-10-10 17:35:21 +0100

    added eager loading and test

---
+++ models/Item.js
@@ -0,0 +1,19 @@
+const { sequelize, DataTypes, Model } = require("../db");
+// const { Sequelize, DataTypes, Model } = require("sequelize");
+
+// TODO - create a Menu model
+class Item extends Model {} 
+Item.init(
+  {
+    name: DataTypes.STRING,
+    image: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    vegetarian: DataTypes.BOOLEAN,
+  },
+  {
+    sequelize: sequelize,
+    modelName: "Item",
+  }
+);
+
+module.exports = {Item};
